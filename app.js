77e222afa847ffd8219ade20443671e1d155e560
Audrey Schmook <audrey.schmook@gmail.com>
2018-06-02 12:28:55 -0500

    updated user routes

--- app.js
+++ app.js
@@ -23,6 +23,17 @@ app.get('/users', function(req,res){
   res.send("Print out user list.");
 });
 
+app.get('/users/:username', function(req,res){
+  var name = req.params.username;
+  res.send("Hello " + name + ", your request is being processed...");
+})
+
+app.get('/users/:username/:location', function(req,res){
+  var name = req.params.username;
+  var loc = req.params.location;
+  res.send("Hello " + name + ", you\'re at " + loc);
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+router.get('/', function(req,res){
+  res.send("Print out user list.");
 });
+
+router.get('/:username', function(req,res){
+  var name = req.params.username;
+  res.send("Hello " + name + ", your request is being processed...");
+})
+
+router.get('/:username/:location', function(req,res){
+  var name = req.params.username;
+  var loc = req.params.location;
+  res.send("Hello " + name + ", you\'re at " + loc);
+})
 
 module.exports = router;
